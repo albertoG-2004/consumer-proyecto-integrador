b445e4ca0e793021977dfabdd0e3a16fffcdbd20
Luis <alberto.gutierrezcetis138@gmail.com>
2024-07-20 19:10:29 -0600

    Añadida escucha de la cola de tokens

--- consumer.js
+++ consumer.js
@@ -4,14 +4,16 @@ import axios from 'axios';
 
 dotenv.config();
 
+let token = '';
+
 async function consumeQueue(queueConfig) {
-    const { url, exchange, queueName, apiEndpoint } = queueConfig;
+    const { url, exchange, queueName, apiEndpoint, isTokenQueue } = queueConfig;
 
     try {
         const conn = await amqp.connect(url);
         const channel = await conn.createChannel();
 
-        await channel.assertExchange(exchange, 'topic', { durable: true });
+        await channel.assertExchange(exchange, 'direct', { durable: true });
         const queue = await channel.assertQueue(queueName, { exclusive: false });
         await channel.bindQueue(queue.queue, exchange, '');
 
@@ -19,18 +21,30 @@ async function consumeQueue(queueConfig) {
 
         channel.consume(queue.queue, async (mensaje) => {
             if (mensaje !== null) {
-                console.log(`Message received from ${queueName}: ${mensaje.content.toString()}`);
+                const messageContent = mensaje.content.toString();
+                // console.log(`Message received from ${queueName}: ${messageContent}`);
+                const data = JSON.parse(messageContent);
 
-                try {
-                    const data = JSON.parse(mensaje.content.toString());
+                if (isTokenQueue) {
                     if (data) {
-                        const response = await axios.post(apiEndpoint, data);
-                        console.log(`Response from API (${queueName}):`, response.data);
+                        token = data;
+                        console.log("Token updated");
                     } else {
-                        console.log("Data no content");
+                        console.log("No token found in message");
                     }
-                } catch (error) {
-                    console.log(`Error sending to API (${queueName}):`, error);
+                } else {
+                    try {
+                        const config = {
+                            headers: {
+                                Authorization: token,
+                            }
+                        };
+
+                        const response = await axios.post(apiEndpoint, data, config);
+                        console.log(`Response from API (${queueName}):`, response.data);
+                    } catch (error) {
+                        console.log(`Error sending to API (${queueName}):`, error);
+                    }
                 }
             }
         }, { noAck: true });
@@ -44,13 +58,21 @@ const queueConfigs = [
         url: process.env.URL,
         exchange: process.env.EXCHANGE_B,
         queueName: process.env.QUEUE_B,
-        apiEndpoint: process.env.ENDPOINT_B
+        apiEndpoint: process.env.ENDPOINT_B,
+        isTokenQueue: false
     },
     {
         url: process.env.URL,
         exchange: process.env.EXCHANGE_M,
         queueName: process.env.QUEUE_M,
-        apiEndpoint: process.env.ENDPOINT_M
+        apiEndpoint: process.env.ENDPOINT_M,
+        isTokenQueue: false
+    },
+    {
+        url: process.env.URL,
+        exchange: process.env.EXCHANGE_T,
+        queueName: process.env.QUEUE_T,
+        isTokenQueue: true
     }
 ];
 
